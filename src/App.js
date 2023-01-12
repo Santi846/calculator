b002ce5ff9c81049b1d60d936f6adeeda953a2da
Santiago Arocha <arochas846@gmail.com>
2023-01-12 17:58:13 -0300

    Styling calculator

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='title'>Web Calculator</h1>
-      <div></div>
+      <div className='container_calculator'></div>
     </div>
   );
 }
